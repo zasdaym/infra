5bb11738bb7194d98d0e2b2a408e3e9cb1371d7c
Zasda Yusuf Mikail <zasdaym@gmail.com>
2022-04-04 10:56:21 +0700

    Add default security list to network module

--- terragrunt/modules/network/main.tf
+++ terragrunt/modules/network/main.tf
@@ -9,15 +9,40 @@ module "vcn" {
   create_nat_gateway      = true
 }
 
+resource "oci_core_security_list" "private" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+
+  display_name = "private"
+
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
+}
+
+resource "oci_core_security_list" "public" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+
+  display_name = "public"
+
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
+}
+
 resource "oci_core_subnet" "private" {
   compartment_id = var.compartment_id
   vcn_id         = module.vcn.vcn_id
   cidr_block     = var.private_subnet
 
-  display_name               = "${var.vcn_name}-private-subnet"
+  display_name               = "private"
   dns_label                  = "private"
   route_table_id             = module.vcn.nat_route_id
   prohibit_public_ip_on_vnic = true
+  security_list_ids          = [oci_core_security_list.private.id]
 }
 
 resource "oci_core_subnet" "public" {
@@ -25,8 +50,9 @@ resource "oci_core_subnet" "public" {
   vcn_id         = module.vcn.vcn_id
   cidr_block     = var.public_subnet
 
-  display_name               = "${var.vcn_name}-public-subnet"
+  display_name               = "public"
   dns_label                  = "public"
   route_table_id             = module.vcn.ig_route_id
   prohibit_public_ip_on_vnic = false
+  security_list_ids          = [oci_core_security_list.public.id]
 }
