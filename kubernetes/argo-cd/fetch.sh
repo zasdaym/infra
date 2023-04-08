e972a4b8667bb067439c1b78e7db491aec4c191c
Zasda Yusuf Mikail <zasdaym@gmail.com>
2023-04-08 14:27:47 +0700

    Add argo-cd

---
+++ kubernetes/argo-cd/fetch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+function main() {
+    local helm_repo="https://argoproj.github.io/argo-helm"
+    local chart_name="argo-cd"
+    local chart_version="5.28.1"
+
+    helm pull \
+        --repo "${helm_repo}" \
+        --version "${chart_version}" \
+        --untar \
+        "${chart_name}"
+
+    cp "${chart_name}/Chart.yaml" .
+    cp -r "${chart_name}/templates" .
+    rm -r "${chart_name}"
+    rm -r "${chart_name}-${chart_version}.tgz"
+}
+
+main
