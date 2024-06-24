abbd0f04c7403b8f61c786c1dfa0b6692a4d5433
Zasda Yusuf Mikail <zasdaym@gmail.com>
2024-06-24 22:19:45 +0700

    Fix sync.sh

--- sync.sh
+++ sync.sh
@@ -5,17 +5,20 @@ set -o nounset
 set -o pipefail
 
 main() {
-    local -r base_dir="/srv/nomad"
-
-    expected_jobs=$(ls "$base_dir" | grep .nomad.hcl | cut -d '.' -f 1)
+    existing_jobs=$(nomad job status -short | awk 'NR>1 {print $1}')
     declare -A should_keep
+    for job in $existing_jobs; do
+        should_keep["$job"]=0
+    done
+
+    local -r base_dir="/srv/nomad"
+    expected_jobs=$(ls "$base_dir" | grep .nomad.hcl | cut -d '.' -f 1)
     for job in $expected_jobs; do
         should_keep["$job"]=1
     done
 
-    existing_jobs=$(nomad job status -short | awk 'NR>1 {print $1}')
     for job in $existing_jobs; do
-        if [[ -n "${should_keep[$job]}" ]]; then
+        if [[ "${should_keep[$job]}" -eq 1 ]]; then
             continue
         fi
         nomad job stop -purge "$job"
